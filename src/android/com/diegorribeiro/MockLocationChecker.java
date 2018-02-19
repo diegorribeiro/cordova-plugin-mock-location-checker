eddf8271fac0cc29026d537bda45b17eb7bd00da
Diego Ribeiro <diego.ribeiro@squadra.com.br>
2018-02-19 18:40:51 -0300

    Correções

--- src/android/com/diegorribeiro/MockLocationChecker.java
+++ src/android/com/diegorribeiro/MockLocationChecker.java
@@ -3,31 +3,35 @@ package com.diegorribeiro;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+
 import android.provider.Settings.Secure;
+import android.location;
 
 public class MockLocationChecker extends CordovaPlugin {
+
+    //private location;
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
 
-        /*
         if (action.equals("check")) {
           if (android.os.Build.VERSION.SDK_INT < 18) {
-            if (Settings.Secure.getString(currentActivity.getContentResolver(), Settings.Secure.ALLOW_MOCK_LOCATION).equals("0"))
-               callbackContext.success("false");
-            else
-               callbackContext.success("true");
+            if (Secure.getString(this.cordova.getActivity().getContentResolver(), Secure.ALLOW_MOCK_LOCATION).equals("0")){
+              callbackContext.success("false");
+            }else{
+              callbackContext.success("true");
+            }
           } else {
-            callbackContext.success(location.isFromMockProvider());
+            //callbackContext.success(location.isFromMockProvider());
+            callbackContext.success("false");
           }
-            return true;
+          return true;
         }else {
             return false;
         }
 
-        */
-
+        /*
         if (action.equals("check")) {
           if (Secure.getString(this.cordova.getActivity().getContentResolver(), Secure.ALLOW_MOCK_LOCATION).equals("0")) {
             callbackContext.success("false");
@@ -38,5 +42,8 @@ public class MockLocationChecker extends CordovaPlugin {
         } else {
            return false;
         }
+
+        */
+
     }
 }
